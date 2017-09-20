4a8c248b6ddd19a60a2065c51c8935383b0c154a
nervehammer <nervehammer.suraj@gmail.com>
2017-09-20 12:01:09 +0000

    products: Add land

---
+++ products/land/pa_land.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#               2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Check for the target product.
+ifeq (pa_land,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Include ParanoidAndroid common configuration
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit from our common CAF device tree.
+include device/qcom/common/common.mk
+
+# Inherit from land device
+$(call inherit-product, device/xiaomi/land/device.mk)
+
+include vendor/pa/main.mk
+# Device identifier. This must come after all inclusions
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := land
+PRODUCT_NAME := pa_land
+PRODUCT_MODEL := Redmi 3S
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := land
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/V8.5.3.0.MALMIED:user/release-keys \
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V8.5.3.0.MALMIED release-keys"
+
+TARGET_VENDOR := Xiaomi
+
+endif
